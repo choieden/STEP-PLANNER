da832c2d1e8f3565fbe06e748ae579a623281ffa
최영준 <magozin@cyworld.com>
2016-11-23 20:15:28 +0900

    Initial Commit

---
+++ STEP PLANNER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STEP PLANNER
+//
+//  Created by youngjun choi on 2016. 11. 23..
+//  Copyright © 2016년 youngjun choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ STEP PLANNERTests/STEP_PLANNERTests.swift
@@ -0,0 +1,36 @@
+//
+//  STEP_PLANNERTests.swift
+//  STEP PLANNERTests
+//
+//  Created by youngjun choi on 2016. 11. 23..
+//  Copyright © 2016년 youngjun choi. All rights reserved.
+//
+
+import XCTest
+@testable import STEP_PLANNER
+
+class STEP_PLANNERTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
